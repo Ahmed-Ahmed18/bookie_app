4432e21dee237155b5ce0524754a44af9a836736
Ahmed Ahmed <ahmed.lola2018@gmail.com>
2025-04-06 09:37:41 +0200

    change them & adding text animation

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,29 @@
 import 'package:bookie_app/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+
+    super.initState();
+    animationController=  AnimationController(
+        vsync:this,
+        duration: Duration(seconds: 1) );
+    slidingAnimation= Tween<Offset>(
+        begin: const Offset(0, 10) , end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -11,6 +31,15 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
+         AnimatedBuilder(animation: slidingAnimation,
+             builder: (context,_){
+              return SlideTransition(
+                 position: slidingAnimation,
+                 child: Text('Read Free Books',textAlign: TextAlign.center,
+                   style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+               );
+             }),
+
 
       ],
     );
